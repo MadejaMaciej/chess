be673fbf4d8818665e24de239da5cfee0c10f018
MadejaMaciej <48794984+MadejaMaciej@users.noreply.github.com>
2021-11-03 16:49:52 +0100

    created game model

--- api/models/games.js
+++ api/models/games.js
@@ -10,12 +10,38 @@ const Game = mongoose.model('Game', new mongoose.Schema({
     gameType: {
         type: String,
         required: true
+    },
+    pgn: {
+        type: String,
+        required: true
+    },
+    fens: {
+        type: Array,
+        required: true
+    },
+    players: {
+        type: Object,
+        required: true
+    },
+    gameSettings: {
+        type: Object,
+        required: true
+    },
+    logs: {
+        type: Array,
+        required: true
     }
 }))
  
 function validateGame(game) {
     const schema = Joi.object({
-        
+           UUID: Joi.string().required(),
+           gameType: Joi.string().required(),
+           pgn: Joi.string().required(),
+           fens: Joi.array().required(),
+           players: Joi.object().required(),
+           gameSettings: Joi.object().required(),
+           logs: Joi.array().required()
     })
     const validation = schema.validate(game)
     return validation;
